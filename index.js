6fd026b76f4664a281dd7ceb3cf594039a5137c8
Wadimx199789 <martinkewichwadim@gmail.com>
2022-08-27 20:38:01 +0300

    feat: add insertPageContent

--- index.js
+++ index.js
@@ -4,13 +4,32 @@ import "./styles/style.css";
 
 const defaultRef = 'https://google.com/';
 const languageParameterName = 'lang';
-const availableLanguages = ['en','es','fr','ja','nl','ru','zh'];
+const availableLanguages = ['en', 'es', 'fr', 'ja', 'nl', 'ru', 'zh'];
 const defaultLanguage = 'en';
 const contentContinueBtn = document.querySelector(".content__continue-btn");
 const contentPricesElements = [...document.querySelectorAll(".content__prices-item")];
 
 contentPricesElements.forEach((priceElement) => {
     priceElement.addEventListener("click", () => handlePriceElementClick(priceElement));
+});
+
+window.addEventListener("resize",()=>{
+    const language = window.navigator.language.split('-')[0];
+    if (!(language === 'ru' || language === 'es' || language === 'fr' || language === 'nl')){
+        const decreasedFontDocument = document.documentElement.classList.contains("decrease-size");
+        if(decreasedFontDocument){
+            document.documentElement.classList.remove("decrease-size")
+        }
+
+        // if(!window.innerHeight < 736 && (language === 'fr' || language === 'ru') ){
+        //     const decreasedContentFeatures = document.querySelector(".content__features").classList.contains("decrease-features-margin");
+        //     if(decreasedContentFeatures){
+        //         document.querySelector(".content__features").classList.remove("decrease-features-margin");
+        //     }
+        // }
+
+    }
+    
 })
 
 initPage();
@@ -19,7 +38,7 @@ async function initPage() {
     setButtonRef();
     const currentLanguage = getLanguageParameterFromUrl();
     const pageContent = await loadContent(currentLanguage);
-    insertPageContent(pageContent);
+    insertPageContent(await pageContent.json())
 }
 
 function setButtonRef(priceElement) {
@@ -44,11 +63,11 @@ function getLanguageParameterFromUrl() {
     const params = new URLSearchParams(window.location.search);
     let urlLanguage = params.get(languageParameterName);
 
-    if(!urlLanguage || !availableLanguages.find(lang => lang === urlLanguage)) {
+    if (!urlLanguage || !availableLanguages.find(lang => lang === urlLanguage)) {
         urlLanguage = getBrowserLanguage();
     }
 
-    if(!availableLanguages.find(lang => lang === urlLanguage)) {
+    if (!availableLanguages.find(lang => lang === urlLanguage)) {
         urlLanguage = defaultLanguage;
     }
 
@@ -66,11 +85,48 @@ function getBrowserLanguage() {
 function parseLanguage(language) {
     return language.split('-')[0];
 }
+function setPrice(element) {
+    const containerOfElement = element.parentNode;
+    const containerCost = Number(containerOfElement.querySelector(".content__prices-cost").dataset.price.substring(1));
+    if (containerOfElement.classList.contains("content__prices-item--annually")) {
+        element.innerHTML = element.innerHTML.replace('{{price}}/', `${Math.floor((containerCost / 12).toFixed(3) * 100) / 100} `);
+    }
+    if (containerOfElement.classList.contains("content__prices-item--monthly")) {
+        element.innerHTML = element.innerHTML.replace('{{price}}/', `${containerCost} `);
+    }
+}
+
+function scaleContent(language){
+    if (language === 'ru' || language === 'es' || language === 'fr' || language === 'nl') {
+        const contentFeatures = document.querySelector(".content__features");
+        document.documentElement.classList.add('decrease-size');
+        if (window.innerHeight < 736 && (language === 'fr' || language === 'ru')){
+            contentFeatures.classList.add("decrease-features-margin");
+        }
+
+    }
+}
 
 async function loadContent(language) {
-    return await await fetch(`./assets/languages/${language}.json`).json();
+    await scaleContent(language);
+    return await fetch(`./assets/languages/${language}.json`);
 }
 
 function insertPageContent(pageContent) {
+    const variableElements = document.querySelectorAll(".content__features-item");
+    const TextElements = document.querySelectorAll("[data-variable]");
+    for (let element of TextElements) {
+        const attrVariable = element.dataset.variable;
+        element.innerHTML = pageContent[attrVariable];
+        if (pageContent.hasOwnProperty(attrVariable)) {
+            if (element.hasAttribute("data-price")) {
+                element.innerHTML = (element.innerHTML.indexOf('{{price}}')) ? element.innerHTML.replace('{{price}}', `${element.dataset.price} `) : element.innerHTML.replace('{{price}}/', `${element.dataset.price} `);
+            }
+            if (element.hasAttribute("data-calc")) {
+                setPrice(element)
+            }
 
+        }
+
+    }
 }
